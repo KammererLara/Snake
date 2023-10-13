969daafbb5e7c0f13df744c65042010334233778
Human-018 <2210859028@fh-burgenland.at>
2023-10-13 11:03:31 +0200

    tried to do the body --> not a success yet (nothing works at the moment, LITERALLY NOTHING)

--- snake.js
+++ snake.js
@@ -17,15 +17,22 @@ const directions = {
     right: "ArrowRight",
     down: "ArrowDown",
     left: "ArrowLeft"
-}
+};
 let head = {
     x: 0,
     y: 0,
     direction: directions.down
 };
 
-let body = {
-    parts: 0
+let body = [];
+
+let bodyEmojis = [];
+
+let newBodyPart = false;
+
+let bodypart = {
+    x: 0,
+    y: 0
 };
 
 
@@ -67,8 +74,18 @@ function changeDirection(event) {
 }
 
 function moveSnake() {
-    // console.log("x: " + head.x);
-    // console.log("y: " + head.y);
+    console.log("x: " + head.x);
+    console.log("y: " + head.y);
+    for (let i=body.length -1; i > 0; i--) {
+        body[i].x= body[i-1].x;
+        body[i].y= body[i-1].y;
+    }
+
+    if (body.length > 0) {
+        body[0].x = head.x;
+        body[0].y = head.y;
+    }
+
     switch (head.direction) {
         case directions.right:
             head.x = head.x+1;
@@ -86,22 +103,48 @@ function moveSnake() {
     headEmoji.style.left = `${head.x*sectionsize}px`;
     headEmoji.style.top = `${head.y*sectionsize}px`;
 
+    for (let i = 0; i < body.length; i++) {
+        bodyEmojis[i].style.left = `${body[i].x*sectionsize}px`;
+        bodyEmojis[i].style.top = `${body[i].y*sectionsize}px`;
+    }
+
+    if (newBodyPart == true ) {
+        if (body.length == 1) {
+            bodyEmojis.push(bodyEmoji);
+            bodyEmoji.style.display= "block";
+        } else if (body.length > 1){
+            bodyEmojis.push(bodyEmoji.cloneNode())
+        }
+        bodyEmoji.style.top= `${body[0].y*sectionsize}px`;
+        bodyEmoji.style.left= `${body[0].x*sectionsize}px`;
+        console.log("x="+ body[0].x + " , y= " + body[0].y);
+        newBodyPart = false;
+    }
+
+    //Wenn man stirbt
     if (head.x<0 || head.x>sectionCount || head.y<0 || head.y>sectionCount) {
         console.log("clear");
         clearInterval(intervalId);
         gameover.style.display="block";
-        gameoverscore.textContent = "Score: " + body.parts;
+        gameoverscore.textContent = "Score: " + body.length;
         score.style.display="none";
         headEmoji.style.display="none";
         brett.style.display="none";
         apfelEmoji.style.display="none";
     }
 
+    //Wenn man Apfel frisst
     if (head.x == apple.x && head.y == apple.y) {
-        body.parts = body.parts+1;
-        score.textContent = "Score: " + body.parts;
+       // body.parts = body.parts+1;
         createApple();
-        if (body.parts % 3 == 0) {
+        body.push(bodypart);
+        console.log(body)
+        //if (body.length == 1) {
+          newBodyPart= true;
+      //  }
+
+        score.textContent = "Score: " + body.length;
+        if (body.length % 3 == 0) {
             tempo = tempo*0.5;
         }
     }
